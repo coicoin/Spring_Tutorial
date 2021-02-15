ff5fac1a8c3d55c1f2fe0e50f7da21e6f151e7e8
Oleg Chernousov <Oleg.Chernousov@icl-services.com>
2021-02-15 10:19:04 +0300

    logs were removed from LoginService

--- modul_1.0/homework/simple_mvc_1.0/src/main/java/org/example/app/services/LoginService.java
+++ modul_1.0/homework/simple_mvc_1.0/src/main/java/org/example/app/services/LoginService.java
@@ -15,9 +15,7 @@ public class LoginService {
     private final Logger logger = Logger.getLogger(LoginService.class);
 
     public boolean authenticate(LoginForm loginForm, List<User> users) {
-        logger.info("Users: " + users);
         logger.info("try auth with user-form: " + loginForm);
-        logger.info("Users[1]]: " + users.get(1));
         return users.stream().filter(Objects::nonNull)
                 .anyMatch(u -> StringUtils.equals(loginForm.getUsername(), u.getUsername())
                         && StringUtils.equals(loginForm.getPassword(), u.getPassword()));
