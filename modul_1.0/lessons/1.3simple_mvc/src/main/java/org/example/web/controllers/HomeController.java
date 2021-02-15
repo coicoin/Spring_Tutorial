10c9e00f6d63d82a91cc9323f48c81fe30fc205a
Oleg Chernousov <Oleg.Chernousov@icl-services.com>
2021-02-15 10:06:30 +0300

    lessons of the first module were added

---
+++ modul_1.0/lessons/1.3simple_mvc/src/main/java/org/example/web/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package org.example.web.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+//для распознования как бин
+@Controller
+public class HomeController {
+
+    private Logger logger = Logger.getLogger(HomeController.class);
+
+    //аннотация запроса
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView home() {
+        logger.info("GET /home returns index.html");
+        return new ModelAndView("index");
+    }
+}
---
+++ modul_1.0/lessons/1.7simple_mvc/src/main/java/org/example/app/services/BookService.java
@@ -0,0 +1,31 @@
+package org.example.app.services;
+
+import org.example.web.dto.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final ProjectRepository<Book> bookRepo;
+
+    //@Autowired чтобы bookRepo подтянулся в BookService в качестве зависимости
+    @Autowired
+    public  BookService(BookRepository<Book> bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepo.retreiveAll();
+    }
+
+    public void saveBook(Book book) {
+        bookRepo.store(book);
+    }
+
+    public boolean removeBookById(Integer bookIdToRemove) {
+        return bookRepo.removeItemById(bookIdToRemove);
+    }
+}
