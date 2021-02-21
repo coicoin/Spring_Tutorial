3e1f910f9509f6f7b574c1e4d1b16688e578e0a6
Oleg Chernousov <Oleg.Chernousov@icl-services.com>
2021-02-21 14:30:09 +0300

    modul_2.0 lesson 1 about bean was added

---
+++ modul_2.0/lessons/2.1simple_mvc/src/main/java/org/example/web/controllers/LoginController.java
@@ -0,0 +1,57 @@
+package org.example.web.controllers;
+
+import org.apache.log4j.Logger;
+import org.example.app.services.LoginService;
+import org.example.web.dto.LoginForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//для распознования как бин
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+
+    private final Logger logger = Logger.getLogger(LoginController.class);
+    //loginService будет бином внедренным в контроллер в качестве зависимости
+    private LoginService loginService;
+
+    //если not autowired, то добавляем @Service в LoginService класс
+    @Autowired //указывает на то, что это бин, который внедрен в контроллер в качестве зависимости
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    //аннотация запроса
+    /*@GetMapping(value = "/login")
+    public ModelAndView login() {
+        logger.info("GET /home returns login_page.html");
+        return new ModelAndView("login_page");
+    }*/
+
+    @GetMapping
+    //добавление model в качестве аргумента
+    public String login(Model model) {
+        logger.info("GET /home returns login_page.html");
+        //в loginForm записывается вводимое имя и пароль для передачи в метод authenticate
+        model.addAttribute("loginForm", new LoginForm());
+        return "login_page";
+    }
+
+    @PostMapping("/auth") // "/login/auth" можно сократить указав в классе аннотацию @RequestMapping
+    // принимает вводимое имя и пароль в качестве аргумента LoginForm
+    public String authenticate(LoginForm loginForm) {
+        //if return true перенаправляем пользователя по /books/shelf или возвращаем на /login
+        if (loginService.authenticate(loginForm)) {
+            logger.info("Login OK redirect to book shelf");
+            return "redirect:/books/shelf";
+        } else {
+            logger.info("Login FAIL redirect back to login");
+            return "redirect:/login";
+        }
+    }
+
+}
