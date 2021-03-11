52ec7bf06a84c18939c0eeb8887f61f28b6cbeef
= <Oleg.Chernousov@icl-services.com>
2021-03-11 23:48:24 +0300

    lang was added to modul 4

---
+++ modul_4.0/lessons/4.2MyBookShopApp/MyBookShopApp/src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -0,0 +1,29 @@
+package com.example.MyBookShopApp.controllers;
+
+import com.example.MyBookShopApp.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
+
+@Controller
+@RequestMapping("/bookshop")
+public class MainPageController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/main")
+    public String mainPage(Model model) {
+        model.addAttribute("bookData", bookService.getBooksData());
+        model.addAttribute("searchPlaceholder", "new search placeholder");
+        return "index";
+    }
+}
