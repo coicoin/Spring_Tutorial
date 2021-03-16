e3f7be5275837b48026ab697b467a0bdff8747f8
= <Oleg.Chernousov@icl-services.com>
2021-03-17 00:18:49 +0300

    lesson 4.3 was added with syntax and operators

---
+++ modul_4.0/lessons/4.3MyBookShopApp/MyBookShopApp/src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -0,0 +1,34 @@
+package com.example.MyBookShopApp.controllers;
+
+import com.example.MyBookShopApp.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/bookshop")
+public class MainPageController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public MainPageController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/main")
+    public String mainPage(Model model) {
+        model.addAttribute("bookData", bookService.getBooksData());
+        model.addAttribute("searchPlaceholder", "new search placeholder");
+        //model.addAttribute("serverTime", new SimpleDateFormat("hh:mm:ss").format(new Date()));
+        model.addAttribute("serverTime", new Date());
+        model.addAttribute("placeholderTextPart2", "SERVER");
+        model.addAttribute("messageTemplate", "searchbar.placeholder2");
+        return "index";
+    }
+}
